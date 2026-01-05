b536b48b5077a2544ed5360db0f015b69db8a8d8
bjavohir2197 <bjavohir2197@gmail.com>
2026-01-05 12:54:01 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import matematik
+
+a = 10
+b = 5
+
+res1 = matematik.qoshish(a, b)
+print(res1)
+res2 = matematik.ayirish(a, b)
+print(res2)
+res3 = matematik.kopaytirish(a, b)
+print(res3)
+res4 = matematik.bolish(a, b)
+print(res4)
